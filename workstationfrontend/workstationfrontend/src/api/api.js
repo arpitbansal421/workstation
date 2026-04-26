1fc09ead2563dbadaa6af15de67d901928b68ccd
Arpit Bansal <arpitbansal421@gmail.com>
2026-04-27 01:15:30 +0530

    Api end point changed

--- workstationfrontend/workstationfrontend/src/api/api.js
+++ workstationfrontend/workstationfrontend/src/api/api.js
@@ -1,16 +1,16 @@
 const BASE_URL = "https://workstation-zo05.onrender.com";
 
 export const getWorkerMetrics = async () => {
-  const res = await fetch(`${BASE_URL}/getWorkerMetrics`);
+  const res = await fetch(`${BASE_URL}/v1/api/getWorkerMetrics`);
   return res.json();
 };
 
 export const getWorkstationMetrics = async () => {
-  const res = await fetch(`${BASE_URL}/getWorkstationMetrics`);
+  const res = await fetch(`${BASE_URL}/v1/api/getWorkstationMetrics`);
   return res.json();
 };
 
 export const getFactoryMetrics = async () => {
-  const res = await fetch(`${BASE_URL}/getFactoryMetrics`);
+  const res = await fetch(`${BASE_URL}/v1/api/getFactoryMetrics`);
   return res.json();
 };
